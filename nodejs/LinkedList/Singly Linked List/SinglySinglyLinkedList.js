c2783b9234db07b4c6c95c7e48ebb94ecfb60294
Nitin <chdnitinsharma@gmail.com>
2024-04-12 13:54:14 +0530

    add display function

--- nodejs/LinkedList/Singly Linked List/SinglySinglyLinkedList.js
+++ nodejs/LinkedList/Singly Linked List/SinglySinglyLinkedList.js
@@ -15,35 +15,36 @@ function addData(data){
  if(!head){
   head = new Node(data);  
 }else{ 
-
  let current = head;
-  let  current_next = head;
+ let  current_next = head;
  while(current_next){
-     
-     console.log("data")
-      console.log(current_next.data)
-      console.log("data")
         current=current_next;
      current_next=current_next.next;
-   
  }
- console.log("current");
- console.log(current);
- console.log("current");
     
  if(current){
    current.next =  new Node(data);
-  console.log(current);   
  }
 
 } 
 
 }
 
+function displayList(){
+     let  current_next = head;
+ while(current_next){
+     
+      console.log(current_next.data)
+        current=current_next;
+     current_next=current_next.next;
+ }
+}
+
 addData(45);
-addData(145)
-addData(1145)
-addData(245)
-addData(45)
-console.log( JSON.stringify(head ));
+addData(145);
+addData(1145);
+addData(245);
+addData(45);
+
+displayList();
 
