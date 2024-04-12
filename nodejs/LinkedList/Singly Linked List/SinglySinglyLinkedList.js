321a41a290ed56858898ec5c9a1a6a2e6a4c9e34
Nitin <chdnitinsharma@gmail.com>
2024-04-12 13:51:20 +0530

    fix

--- nodejs/LinkedList/Singly Linked List/SinglySinglyLinkedList.js
+++ nodejs/LinkedList/Singly Linked List/SinglySinglyLinkedList.js
@@ -2,77 +2,48 @@
 // Write, Edit and Run your Javascript code using JS Online Compiler
 
 class Node{
-    constructor(data){
-        this.data = data;
-        this.next = null;
-    }
+  constructor(data){
+      this.data = data;
+      this.next = null;
+  }
 }
 
 
 let head = null;
 
-if(!head){
-  head = new Node(12);
-}
+function addData(data){
+ if(!head){
+  head = new Node(data);  
+}else{ 
 
-console.log(head)
-if(head){
-    let  current = new Node(14);
- head.next = current;  
-}
-console.log(head)
-if(head){
-//     let  current = new Node(16);
-//  head.next.next = current;  
- 
-   let current = head;
-    let  current_next = head;
-   while(current_next){
-       
-       console.log("current_next.data")
-        console.log(current_next.data)
-        console.log("current_next.data")
-          current=current_next;
-       current_next=current_next.next;
+ let current = head;
+  let  current_next = head;
+ while(current_next){
      
-   }
-   console.log("current");
-   console.log(current);
-   console.log("current");
-      
-   if(current){
-     current.next =  new Node(16);
-    console.log(current);   
-   }
- 
-}
-console.log(head);
+     console.log("data")
+      console.log(current_next.data)
+      console.log("data")
+        current=current_next;
+     current_next=current_next.next;
+   
+ }
+ console.log("current");
+ console.log(current);
+ console.log("current");
+    
+ if(current){
+   current.next =  new Node(data);
+  console.log(current);   
+ }
 
+} 
 
-if(head){
-//     let  current = new Node(16);
-//  head.next.next = current;  
- 
-   let current = head;
-    let  current_next = head;
-   while(current_next){
-       
-       console.log("current_next.data")
-        console.log(current_next.data)
-        console.log("current_next.data")
-          current=current_next;
-       current_next=current_next.next;
-     
-   }
-   console.log("current");
-   console.log(current);
-   console.log("current");
-      
-   if(current){
-     current.next =  new Node(18);
-    console.log(current);   
-   }
- 
 }
-console.log(JSON.stringify(head))
+
+addData(45);
+addData(145)
+addData(1145)
+addData(245)
+addData(45)
+console.log( JSON.stringify(head ));
 
